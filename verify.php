b3c622fb59ea6bddbe73f5263f0d594d48e2aa09
Khuekphon <35328255+Khuekphon@users.noreply.github.com>
2018-01-12 12:31:58 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,7 +1,15 @@
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('<channel access token>');
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '<channel secret>']);
+<?php
+$access_token = 'XXXXXXXXXXXXXXXXXXXXXXX';
 
-$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
-$response = $bot->replyMessage('<replyToken>', $textMessageBuilder);
+$url = 'https://api.line.me/v1/oauth/verify';
 
-echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
+$headers = array('Authorization: Bearer ' . $access_token);
+
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+$result = curl_exec($ch);
+curl_close($ch);
+
+echo $result;
