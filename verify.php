b554e4f3811b46f7a66b58a54053afda02eede40
Khuekphon <35328255+Khuekphon@users.noreply.github.com>
2018-01-12 12:22:54 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,9 +1,9 @@
 <?php
-$access_token = 'XXXXXXXXXXXXXXXXXXXXXXX';
+$access_token = 'มหาวิทยาลัยราชภัฏมหาสารคาม';
 
-$url = 'https://api.line.me/v1/oauth/verify';
+$url = 'FTAdDbJGsMUtmLTNqwOjUIe7qr0BGt/2y53y5kLiBEletJdkqL14wFVXbeIl5CiqSw6n/iXXq+96GzgacfYNnf3apBSlWAfmey+QSWx2lcrQF87OX5WmzePZar5mos520Tu0IaQvAmOsjbNCSD8+4QdB04t89/1O/w1cDnyilFU=';
 
-$headers = array('Authorization: Bearer ' . $access_token);
+$headers = array('Authorization: Bearer'.$access_token);
 
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
