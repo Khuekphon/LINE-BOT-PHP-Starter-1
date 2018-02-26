d9b33b05202e0b8257b765416196c10b7db7fea0
Khuekphon <35328255+Khuekphon@users.noreply.github.com>
2018-02-26 14:57:50 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -31,6 +31,7 @@ if (!is_null($events['events'])) {
 			$bot=[ ];
 			$bot[]=$messages;
 			$bot[]=$messages;
+			$bot[]=$messages;
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
