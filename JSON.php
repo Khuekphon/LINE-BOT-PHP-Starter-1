3aa94978b6b09f9f7f3b17abd4f80952941b4823
Khuekphon <35328255+Khuekphon@users.noreply.github.com>
2018-02-26 16:40:54 +0700

    Update JSON.php

--- JSON.php
+++ JSON.php
@@ -4,7 +4,6 @@ $sql="select * from chat_TB where chat_question like '%".$_GET["key"]."%'";
 $result = mysqli_query($conn, $sql);
 $row = mysqli_fetch_assoc($result);
 	
-$
 	$messages = [
 		'type' => 'text',
 		'text' => $row['chat_answer']
