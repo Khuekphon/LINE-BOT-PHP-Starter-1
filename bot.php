c3509019ccb52898fe5672b72c8e88fa1911a6b0
Khuekphon <35328255+Khuekphon@users.noreply.github.com>
2018-02-26 15:36:07 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -31,16 +31,14 @@ if (!is_null($events['events'])) {
 				// Make a POST Request to Messaging API to reply to sender
 				$url = 'https://api.line.me/v2/bot/message/reply';
 			
+				
 				$data = [
 					'replyToken' => $replyToken,
-					'messages' => [$messages],
+					'messages' => [$messages,$messages],
 				];
-			$arr = array();
-			$arr[] = $data;
-			$arr[] = $data;
-			$arr[] = $data;
 			
-				$post = json_encode($arr);
+			
+				$post = json_encode($data);
 			
 				$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
