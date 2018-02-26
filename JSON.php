96a6a00ac5e1fce829804792d3230cbfa9c6a05a
Khuekphon <35328255+Khuekphon@users.noreply.github.com>
2018-02-26 16:26:55 +0700

    Add files via upload

---
+++ JSON.php
@@ -0,0 +1,18 @@
+<?php
+include("connect.php");
+$sql="select * from chat_TB where chat_question like '%".$_GET["key"]."%'";
+$result = mysqli_query($conn, $sql);
+$row = mysqli_fetch_assoc($result);
+
+	$messages = [
+		'type' => 'text',
+		'text' => $row['chat_answer']
+	];
+
+	$data = [
+		'replyToken' => "TEST",
+		'messages' => [$messages,$messages],
+	];
+	$post = json_encode($data);
+	
+	echo $post;
